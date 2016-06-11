3a7573311d53ff8183862b03060776b5693056f2
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2016-06-11 17:22:28 +0530

    Function Fundamentals
    - Anonymous functions
    - Function Variable
    - Global Functions

---
+++ 3Function Fundamentals/scripts/script.js
@@ -0,0 +1,80 @@
+/*function printOrder() {
+    console.log('printOrder()');
+};
+
+printOrder();
+*/
+
+
+
+/*function printOrder(orderId) {
+    console.log('orderId => ' + orderId);
+};
+
+printOrder(9116);*/
+
+
+
+/*function printOrder(orderId){
+		 console.log('orderId => '+ orderId);
+};
+printOrder('9116');*/
+
+
+
+
+
+/*printOrder('9116');
+function printOrder(orderId){
+		 console.log('orderId => '+ orderId);
+};*/
+
+
+
+/*function calculateTotalPrice(quantity, price) {
+    console.log('Total Price => ' + quantity * price);
+};
+
+calculateTotalPrice(2,4.00);*/
+
+
+/*function calculateTotalPrice(quantity,price,currency){
+		 console.log(currency);
+}
+calculateTotalPrice(2,4.00);*/
+
+/*function calculateTotalPrice(quantity, price) {
+    return quantity * price;
+};
+
+var totalPrice = calculateTotalPrice(4.0, 2);
+console.log('Total Price =>  ' + totalPrice);*/
+
+
+/*function calculateTotal(quantity, price) {
+    return quantity * price;
+}
+
+var totalPrice = calculateTotal(10, 12);
+console.log('Total Price => ' + totalPrice);
+
+var totalPrice = calculateTotal(10, 120);
+console.log('Total Price => ' + totalPrice);*/
+
+/*function order() {
+
+};
+var orders = order();
+console.log(orders);
+*/
+
+var addingValue = function (a,b) {
+    console.log(a+b);
+};
+console.log(addingValue(10,10));
+console.log(addingValue);
+console.log(typeof addingValue);
+addingValue();
+addingValue(50,50);
+addingValue;
+
